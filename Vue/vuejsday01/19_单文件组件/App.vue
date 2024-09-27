dce244d64641f79fcc6558b35ebd8a9a2524ccfa
chikaoya <92580754+chikaoya@users.noreply.github.com>
2024-09-27 22:02:37 +0800

    Add files via upload

---
+++ Vue/vuejsday01/19_单文件组件/App.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- 因为是template该结构不会影响页面 -->
+<!-- 最外层需要div包裹，否则组件不能使用 -->
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// 引入组件 es6模块化语法
+import School from './School.vue'
+import Student from './Student.vue'
+// 暴露App组件给html网页使用
+export default {
+  name:'App',
+  components:{
+    School,
+    Student
+  }
+}
+</script>
+
+<style>
+
+</style>
